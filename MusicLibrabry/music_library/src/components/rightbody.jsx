cfec85211bc432c73ef9c34f53d4e18ab28f8737
Justin Adams <khisanth05@gmail.com>
2021-04-18 23:03:46 -0400

    Minor - Right Body for filter has been styled, working on getting the submit action to filter the props.data

--- MusicLibrabry/music_library/src/components/rightbody.jsx
+++ MusicLibrabry/music_library/src/components/rightbody.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 
 function RightBody(props){
@@ -12,7 +13,29 @@ function RightBody(props){
             <Container fluid>
                 <Row>
                     <Col>
-                        <h3>Choices</h3>
+                        <h3>Filter By:</h3>
+                        <h4>
+                            <Form action="/action_page.php">
+                                <label>
+                                    Album: 
+                                    <input type="text" name="searchCriteria" /><br/>
+                                </label>
+                                <label>
+                                    Artist: 
+                                    <input type="text" name="searchCriteria" /><br/>
+                                </label>
+                                <label>
+                                    Song Title:
+                                    <input type="text" name="searchCriteria"  /><br/>
+                                </label>
+                                <label>
+                                    Release Year:
+                                    <input type="text" name="searchCriteria"  /><br/><br/>
+                                </label>
+                                <input type="submit" value="Submit"></input>
+                                <input type="reset" value= "Reset"></input>
+                            </Form>
+                        </h4>
                     </Col>
                 </Row>
             </Container>
